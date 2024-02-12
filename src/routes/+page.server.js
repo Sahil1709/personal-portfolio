ca68acda79231c6bc1c644b8053a441c86b3980d
Sahil Gupta <65862376+Sahil1709@users.noreply.github.com>
2024-02-12 21:10:10 +0000

    test from codespaces

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,7 @@
 import { client } from '$lib/sanity.js';
 
 export const load = async ({ params }) => {
+    console.log(params);
     return {
         personalInfo: await client.fetch('*[_type == "personalInfo"][0]'),
         universities: await client.fetch('*[_type == "education"] | order(from desc) [0...2]'),
